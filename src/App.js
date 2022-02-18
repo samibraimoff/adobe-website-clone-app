5b509358447c823abf998d53404d2a138524172e
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-02-18 01:10:27 -0500

    Hero Links component added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './components/header/Header';
 
 import './App.scss';
 import Hero from './components/hero/Hero';
+import HeroLinks from './components/herolinks/HeroLinks';
 
 const App = () => {
   return (
     <Fragment>
       <Header />
       <Hero />
+      <HeroLinks />
     </Fragment>
   );
 };
---
+++ src/components/herolinks/HeroLinks.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './herolinks.scss';
+
+const HeroLinks = () => {
+  return (
+    <section className='heroLinks'>
+      <ul>
+        <li>Creative cloud for:</li>
+        <li>individuals</li>
+        <li>Students & Teachers</li>
+        <li>Business</li>
+      </ul>
+      <span></span>
+    </section>
+  );
+};
+
+export default HeroLinks;
