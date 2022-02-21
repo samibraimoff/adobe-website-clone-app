32f8e8fb2559153e44ab3c9e8c43b881fcf93caf
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-02-20 23:12:32 -0500

    features section

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -5,37 +5,33 @@ const Features = () => {
   return (
     <section className='features'>
       <div className='feature1'>
-        <img src='/assets/photoshop.jpg' alt='' />
-        <h2>Meet the new Photoshop.</h2>
-        <h3>Faster, smarter and easier than ever.</h3>
-        <a href='#'>Learn more</a>
+        <div className='feature1-info'>
+          <h2>Meet the new Photoshop.</h2>
+          <h3>Faster, smarter and easier than ever.</h3>
+          <a href='#'>Learn more</a>
+        </div>
       </div>
 
       <div className='feature2'>
-        <img src='' alt='' />
-        <h2>Take your photos further.</h2>
-        <h3>Creative cloud photography plan.</h3>
-        <a href='#'>Learn more</a>
+        <div className='feature2-info'>
+          <h2>Take your photos further.</h2>
+          <h3>Creative cloud photography plan.</h3>
+          <a href='#'>Learn more</a>
+        </div>
       </div>
 
       <div className='feature3'>
-        <img src='' alt='' />
-        <h2>Video editing. The easy way.</h2>
-        <h3>
-          Take your content further and stand out with the next-level power of
-          Premiere Pro.
-        </h3>
-        <a href='#'>Learn more</a>
+        <div className='feature3-info'>
+          <h2>Video editing. The easy way.</h2>
+          <h3>
+            Take your content further and stand out with the next-level power of
+            Premiere Pro.
+          </h3>
+          <a href='#'>Learn more</a>
+        </div>
       </div>
 
       <div className='feature4'>
-        <img src='' alt='' />
-        <h2>Get it done with acrobat.</h2>
-        <h3>Edit, share and e-sign all of your PDFs - all in one place.</h3>
-        <a href='#'>Start free trial</a>
-      </div>
-
-      <div className='feature5'>
         <h2>Explore our creative apps</h2>
         <ul>
           <li>Creative cloud all apps free trial</li>
@@ -51,6 +47,14 @@ const Features = () => {
           <li>3D and AR</li>
           <li>Social media</li>
         </ul>
+      </div>
+
+      <div className='feature5'>
+        <div className='feature4-info'>
+          <h2>Get it done with acrobat.</h2>
+          <h3>Edit, share and e-sign all of your PDFs - all in one place.</h3>
+          <a href='#'>Start free trial</a>
+        </div>
       </div>
     </section>
   );
